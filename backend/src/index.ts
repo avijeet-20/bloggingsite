7e396f30dfa08af276bf6be09eac314870ed1695
avijeet-20 <avipandey20@gmail.com>
2024-04-16 15:23:49 +0530

    added signup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,25 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 import dotenv from 'dotenv';
 //reading the .env file
-dotenv.config();
-
-
-const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-}).$extends(withAccelerate());
 
 const app = new Hono<{
 	Bindings: {
-		DATABASE_URL: string
+		DATABASE_URL: string,
+    JWT_SECRET:string
 	}
 }>();
 
+
+
+
+
 app.get('api/v1/blog/:id', (c) => {
+  
+
+
   return c.text('Hello Hono!')
 })
 
@@ -24,8 +27,54 @@ app.get('api/v1/blog/bulk', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.post('api/v1/user/signup',(c) => {
-  return c.text('hello')
+app.post('api/v1/user/signup', async (c) => {
+
+  const prisma = await new PrismaClient({
+    datasourceUrl:c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  
+
+  const body = await c.req.json();
+
+  async function digestMessage(message:string) {
+    const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
+    const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8); // hash the message
+    const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join(""); // convert bytes to hex string
+    return hashHex;
+  }
+
+  
+const digestHex = await digestMessage(body.password)
+
+
+  try{
+    const creatadUser = await prisma.user.create({
+      data:{
+        email:body.email,
+        name:body.name,
+        password:digestHex
+      }
+    })
+
+//jwt
+const jwt = sign({id:creatadUser.id},c.env.JWT_SECRET)
+
+    return c.json({
+      message: "user created successfully",
+      token :  jwt
+    })
+
+  }catch(e){
+    return c.text("error while signing up")
+    console.log(e)
+  }
+
+
+ 
+
 })
 app.post('api/v1/user/signin',(c) => {
   return c.text('hello')
