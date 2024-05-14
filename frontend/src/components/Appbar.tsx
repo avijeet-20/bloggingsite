2a05740ef56c0006617d76c4679a4d59f1662663
avijeet-20 <avipandey20@gmail.com>
2024-05-14 14:04:39 +0530

    added appbar functionality

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,10 +1,16 @@
 import { ChangeEvent } from "react"
 import { Avatar } from "./Blogcard"
+import { Link, useNavigate } from "react-router-dom"
 
-export function Appbar({onClick}:{onClick:(e:MouseEventHandler<HTMLButtonElement>) => void}) {
+export function Appbar() {
+ const navigate = useNavigate();
+    function handleClick() {
+        navigate('/publish')
+    }
+
     return <div className="flex justify-between p-4 px-10">
-        <div>Medium</div>
-        <div className="flex justify-between gap-4">  <button onClick={onClick} type="submit" className="inline-flex items-center px-4 py-2.5 text-sm font-medium text-center text-white bg-green-500 rounded-lg ml-4 ">
+       <Link to={'/blogs'}><div>Medium</div></Link> 
+        <div className="flex justify-between gap-4">  <button onClick={handleClick} type="submit" className="inline-flex items-center px-4 py-2.5 text-sm font-medium text-center text-white bg-green-500 rounded-lg ml-4 ">
            new +
        </button><Avatar size="big" authorname="Avijeet"></Avatar></div>
     </div>
